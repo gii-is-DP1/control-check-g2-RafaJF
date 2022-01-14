3c3d96f50edefa37e56c3343a974912e5e703056
Rafa Jiménez <rafaelangeljimenezfer@gmail.com>
2022-01-14 12:00:32 +0100

    Update

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,17 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +20,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Entity
+public class Feeding extends BaseEntity {
+    
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Column(name = "start_date")
+    @NotNull
     LocalDate startDate;
+
+    @NotNull
+    @Min(value = 1)
+    @Column(name = "weeks_duration")
     double weeksDuration;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "pet")
     Pet pet;   
+
+    @ManyToOne
+    @JoinColumn(name = "feedingType")
+    FeedingType feedingType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,9 +3,21 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedingRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedingRepository extends CrudRepository<Feeding,Integer>{
     List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+
+    @Query("SELECT fed FROM FeedingType fed")
+    List<FeedingType> findAllFeedingTypes();
+
+    @Query("SELECT ft FROM FeedingType ft WHERE ft.name LIKE :name")
+    FeedingType getFeedingType(@Param("name") String name);
+
     Optional<Feeding> findById(int id);
     Feeding save(Feeding p);
 }
